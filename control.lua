375fab7f16e519d65c04fadb794a2eabda57854e
Brandon Michael Whitchurch <brandonwhitchurch@gmail.com>
2020-05-02 16:35:29 -0400

    Fixed crash caused by removing a line of code that was not actually extraneous.

--- control.lua
+++ control.lua
@@ -22,6 +22,7 @@ local function spawnChances()
 	local mediumChance = settings.global["ruins-medium-ruin-chance"].value
 	local largeChance = settings.global["ruins-large-ruin-chance"].value
 	local sumChance = smallChance + mediumChance + largeChance
+	local totalChance = sumChance or 1
 	-- now compute cumulative distribution of conditional probabilities for
 	-- spawnType given a spawn occurs.
 	local smallThreshold = smallChance / sumChance * totalChance
