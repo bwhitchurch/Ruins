906e67c26056308093ee242f668d8c37f72552cb
Brandon Michael Whitchurch <brandonwhitchurch@gmail.com>
2020-05-02 22:09:12 -0400

    Renamed all ruins files for lexographic sorting.

--- scripts/largeRuins.lua
+++ scripts/largeRuins.lua
@@ -1,14 +1,14 @@
-local clearArea = require("clearArea").clearArea
+local clearArea = require("clearArea01").clearArea
 local radius = 16
 local l_ruins = {}
 
-table.insert(l_ruins, require("largeRuins.destroyedEnemyFort"))
-table.insert(l_ruins, require("largeRuins.destroyedFort"))
-table.insert(l_ruins, require("largeRuins.earlyGame"))
-table.insert(l_ruins, require("largeRuins.mainBus"))
-table.insert(l_ruins, require("largeRuins.orchard"))
-table.insert(l_ruins, require("largeRuins.walledOrchard"))
-table.insert(l_ruins, require("largeRuins.walledGrotto"))
+table.insert(l_ruins, require("largeRuins.destroyedEnemyFort01"))
+table.insert(l_ruins, require("largeRuins.destroyedFort01"))
+table.insert(l_ruins, require("largeRuins.earlyGame01"))
+table.insert(l_ruins, require("largeRuins.mainBus01"))
+table.insert(l_ruins, require("largeRuins.orchard01"))
+table.insert(l_ruins, require("largeRuins.walledOrchard01"))
+table.insert(l_ruins, require("largeRuins.walledGrotto01"))
 
 local function spawnLargeRuins(center, surface)
 	if clearArea(center, surface, radius) then
