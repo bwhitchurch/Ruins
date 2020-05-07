383540bd670028d9223a1af6a7bbd6c2182311a8
Brandon Michael Whitchurch <brandonwhitchurch@gmail.com>
2020-05-07 13:32:12 -0700

    global.spawnTable is declared only inside the spawnChances() function. Added back on_load to maintain local reference to the global table.

--- control.lua
+++ control.lua
@@ -2,72 +2,64 @@ require("smallRuins")
 require("mediumRuins")
 require("largeRuins")
 
-global.spawnTable = {}
+local spawnTable = {}
 
-local DEBUG = false --used for debug, users should not enable
+local DEBUG = false -- used for debug, users should not enable
 
---function that will return true 'percent' of the time.
-function probability(percent)
-    return math.random() <= percent
-end
+-- function that will return true 'percent' of the time.
+function probability(percent) return math.random() <= percent end
 
 local function spawnChances()
-	local smallChance = settings.global["ruins-small-ruin-chance"].value
-	local mediumChance = settings.global["ruins-medium-ruin-chance"].value
-	local largeChance = settings.global["ruins-large-ruin-chance"].value
-	local sumChance = smallChance + mediumChance + largeChance
-	local totalChance = math.min(sumChance, 1)
-	-- now compute cumulative distribution of conditional probabilities for
-	-- spawnType given a spawn occurs.
-	local smallThreshold = smallChance / sumChance * totalChance
-	local mediumThreshold =
-		mediumChance / sumChance * totalChance + smallThreshold
-	local largeThreshold =
-		largeChance / sumChance * totalChance + mediumThreshold
+    local smallChance = settings.global["ruins-small-ruin-chance"].value
+    local mediumChance = settings.global["ruins-medium-ruin-chance"].value
+    local largeChance = settings.global["ruins-large-ruin-chance"].value
+    local sumChance = smallChance + mediumChance + largeChance
+    local totalChance = math.min(sumChance, 1)
+    -- now compute cumulative distribution of conditional probabilities for
+    -- spawnType given a spawn occurs.
+    local smallThreshold = smallChance / sumChance * totalChance
+    local mediumThreshold = mediumChance / sumChance * totalChance + smallThreshold
+    local largeThreshold = largeChance / sumChance * totalChance + mediumThreshold
 
     global.spawnTable = {small = smallThreshold, medium = mediumThreshold, large = largeThreshold}
+    spawnTable = global.spawnTable
 end
 
 -- Spawn Table won't change as long as settings don't change, so we can compute
 -- once and forget about it.
 script.on_init(spawnChances)
 script.on_event(defines.events.on_runtime_mod_setting_changed, spawnChances)
+script.on_load(function() spawnTable = global.spawnTable end)
+script.on_event(defines.events.on_chunk_generated, function(e)
+    local center = {
+        x = (e.area.left_top.x + e.area.right_bottom.x) / 2,
+        y = (e.area.left_top.y + e.area.right_bottom.y) / 2
+    }
+    if math.abs(center.x) < settings.global["ruins-min-distance-from-spawn"].value and math.abs(center.y) <
+        settings.global["ruins-min-distance-from-spawn"].value then return end -- too close to spawn
+    local spawnType = math.random()
 
-script.on_event(defines.events.on_chunk_generated,
-    function (e)
-        local center = {x=(e.area.left_top.x+e.area.right_bottom.x)/2, y=(e.area.left_top.y+e.area.right_bottom.y)/2}
-        if math.abs(center.x) < settings.global["ruins-min-distance-from-spawn"].value and math.abs(center.y) < settings.global["ruins-min-distance-from-spawn"].value then return end --too close to spawn
-        local spawnTable = global.spawnTable
-        local spawnType = math.random()
+    if spawnType <= spawnTable.small then
+        -- spawn small ruin
+        if DEBUG then game.print("A small ruin was spawned at " .. center.x .. "," .. center.y) end
 
-        if spawnType <= spawnTable.small then
-            --spawn small ruin
-            if DEBUG then
-                game.print("A small ruin was spawned at " .. center.x .. "," .. center.y)
-            end
+        -- random variance so they aren't always chunk aligned
+        center.x = center.x + math.random(-10, 10)
+        center.y = center.y + math.random(-10, 10)
 
-            --random variance so they aren't always chunk aligned
-            center.x = center.x + math.random(-10,10)
-            center.y = center.y + math.random(-10,10)
+        spawnSmallRuins(center, e.surface)
+    elseif spawnType <= spawnTable.medium then
+        -- spawn medium ruin
+        if DEBUG then game.print("A medium ruin was spawned at " .. center.x .. "," .. center.y) end
 
-            spawnSmallRuins(center, e.surface)
-        elseif spawnType <= spawnTable.medium then
-            --spawn medium ruin
-            if DEBUG then
-                game.print("A medium ruin was spawned at " .. center.x .. "," .. center.y)
-            end
+        -- random variance so they aren't always chunk aligned
+        center.x = center.x + math.random(-5, 5)
+        center.y = center.y + math.random(-5, 5)
 
-            --random variance so they aren't always chunk aligned
-            center.x = center.x + math.random(-5,5)
-            center.y = center.y + math.random(-5,5)
-
-            spawnMediumRuins(center, e.surface)
-        elseif spawnType <= spawnTable.large then
-            --spawn large ruin
-            if DEBUG then
-                game.print("A large ruin was spawned at " .. center.x .. "," .. center.y)
-            end
-            spawnLargeRuins(center, e.surface)
-        end
+        spawnMediumRuins(center, e.surface)
+    elseif spawnType <= spawnTable.large then
+        -- spawn large ruin
+        if DEBUG then game.print("A large ruin was spawned at " .. center.x .. "," .. center.y) end
+        spawnLargeRuins(center, e.surface)
     end
-)
+end)
